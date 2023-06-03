088661c7cc8abe1d4a00e050a5abafd64dff5fe0
Booki Park <bkbkgg@gmail.com>
2023-06-03 21:31:56 +0900

    #12 - 알림 기능을 위한 엔티티 필드 수정
    Foreign Key 연결, DB 접근 메서드 추가, NotificationEntity 매핑, Hibernate Types 활용해 meta 필드 json 타입 지정.

---
+++ src/main/java/com/fc/pass/repository/notification/NotificationEntity.java
@@ -0,0 +1,29 @@
+package com.fc.pass.repository.notification;
+
+import com.fc.pass.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "notification")
+public class NotificationEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer notificationSeq;
+
+    private String uuid;
+
+    private NotificationEvent event;
+    private String text;
+    private boolean sent;
+    private LocalDateTime sentAt;
+
+}
