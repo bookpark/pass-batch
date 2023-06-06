c58c471c1b523a62d9c8ef6dd543ceeaf4cd3430
Booki Park <bkbkgg@gmail.com>
2023-06-06 20:01:40 +0900

    #16 - 통계를 위한 엔티티, 리포지토리, 코드 추가

---
+++ src/main/java/com/fc/pass/repository/statistics/StatisticsEntity.java
@@ -0,0 +1,54 @@
+package com.fc.pass.repository.statistics;
+
+import com.fc.pass.repository.booking.BookingEntity;
+import com.fc.pass.repository.booking.BookingStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "statistics")
+public class StatisticsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer statisticsSeq;
+    private LocalDateTime statisticsAt; // 일 단위
+
+    private int allCount;
+    private int attendedCount;
+    private int cancelledCount;
+
+    public static StatisticsEntity create(final BookingEntity bookingEntity) {
+        StatisticsEntity statisticsEntity = new StatisticsEntity();
+        statisticsEntity.setStatisticsAt(bookingEntity.getStatisticsAt());
+        statisticsEntity.setAllCount(1);
+        if (bookingEntity.isAttended()) {
+            statisticsEntity.setAttendedCount(1);
+
+        }
+        if (BookingStatus.CANCELLED.equals(bookingEntity.getStatus())) {
+            statisticsEntity.setCancelledCount(1);
+
+        }
+        return statisticsEntity;
+    }
+
+    public void add(final BookingEntity bookingEntity) {
+        this.allCount++;
+
+        if (bookingEntity.isAttended()) {
+            this.attendedCount++;
+        }
+        if (BookingStatus.CANCELLED.equals(bookingEntity.getStatus())) {
+            this.cancelledCount++;
+        }
+    }
+
+}
